18d9387b005f73447061199218c80e833ca7b13f
takahiro-kani <imasokari_sg@yahoo.co.jp>
2024-01-13 21:12:58 +0900

    kadai_mdn_js追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,12 @@
+const date = new Date();
+const [month, day, year] = [
+  date.getMonth() + 1,
+  date.getDate(),
+  date.getFullYear(),
+];
+
+const formattedDate = `${year}年${month}月${day}日`;
+
+console.log(formattedDate);
+
+
